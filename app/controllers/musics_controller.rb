d644a70720208b0bbbf59a3262d54f10fd3b1086
Magnolis_Mint <ashe.moira@gmail.com>
2018-01-09 00:32:30 +0900

    add controller methods

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,7 +1,19 @@
 class MusicsController < ApplicationController
   def new
+    @music = Music.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 
   def show
   end
+
+  def index; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,15 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
   def show
   end
 end
