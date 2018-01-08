aeacec574317077cceaddbe6cc6b14929ee41449
Magnolis_Mint <ashe.moira@gmail.com>
2018-01-08 23:51:24 +0900

    add music and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'musics/new'
+
+  get 'musics/show'
+
+  get 'users/new'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
